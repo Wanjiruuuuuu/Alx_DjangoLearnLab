e921f5065176837305fda040b080ed03966c9560
Rachel <rachelwanjiru037@gmail.com>
2025-02-23 17:25:03 +0300

    Updated code

--- django_models/relationship_app/tests.py
+++ django_models/relationship_app/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from relationship_app.models import Author, Book, Library, Librarian
+from relationship_app.query_samples import get_books_by_author, get_books_in_library, get_librarian_for_library
 
 class AuthorModelTest(TestCase):
     def setUp(self):
@@ -32,3 +33,24 @@ class LibrarianModelTest(TestCase):
     def test_librarian_creation(self):
         self.assertEqual(self.librarian.name, "Test Librarian")
         self.assertEqual(self.librarian.library.name, "Test Library")
+
+class QuerySamplesTest(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(name="Test Author")
+        self.book1 = Book.objects.create(title="Test Book 1", author=self.author)
+        self.book2 = Book.objects.create(title="Test Book 2", author=self.author)
+        self.library = Library.objects.create(name="Test Library")
+        self.library.books.add(self.book1, self.book2)
+        self.librarian = Librarian.objects.create(name="Test Librarian", library=self.library)
+
+    def test_get_books_by_author(self):
+        books = get_books_by_author("Test Author")
+        self.assertEqual(len(books), 2)
+
+    def test_get_books_in_library(self):
+        books = get_books_in_library("Test Library")
+        self.assertEqual(len(books), 2)
+
+    def test_get_librarian_for_library(self):
+        librarian = get_librarian_for_library("Test Library")
+        self.assertEqual(librarian.name, "Test Librarian")
