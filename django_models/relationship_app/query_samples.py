bafaedf2cfcf3efe004e02a6db7d9a789e021a5b
Rachel <rachelwanjiru037@gmail.com>
2025-02-23 17:17:55 +0300

    Corrected code

--- django_models/relationship_app/query_samples.py
+++ django_models/relationship_app/query_samples.py
@@ -5,12 +5,12 @@ def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
     return Book.objects.filter(author=author)
 
-# Query all libraries that have a specific book
-def get_libraries_with_book(book_title):
-    book = Book.objects.get(title=book_title)
-    return Library.objects.filter(books=book)
-
-# Query all librarians in a specific library
-def get_librarians_in_library(library_name):
+# List all books in a library
+def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    return Librarian.objects.filter(library=library)
+    return library.books.all()
+
+# Retrieve the librarian for a specific library
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
