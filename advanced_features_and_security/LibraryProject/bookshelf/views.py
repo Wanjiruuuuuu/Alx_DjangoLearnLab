261e3873145deab589dea4d57762701316e370d8
Rachel <rachelwanjiru037@gmail.com>
2025-03-01 21:50:01 +0300

    task one

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth.decorators import permission_required
 from .models import Book
 
 # View to list all books
-def list_books(request):
+def book_list(request):
     books = Book.objects.all()
-    return render(request, 'bookshelf/list_books.html', {'books': books})
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 # View to create a new book
 @permission_required('bookshelf.can_create', raise_exception=True)
@@ -15,7 +15,7 @@ def create_book(request):
         author = request.POST['author']
         publication_year = request.POST['publication_year']
         Book.objects.create(title=title, author=author, publication_year=publication_year)
-        return redirect('list_books')
+        return redirect('book_list')
     return render(request, 'bookshelf/create_book.html')
 
 # View to edit an existing book
@@ -27,7 +27,7 @@ def edit_book(request, book_id):
         book.author = request.POST['author']
         book.publication_year = request.POST['publication_year']
         book.save()
-        return redirect('list_books')
+        return redirect('book_list')
     return render(request, 'bookshelf/edit_book.html', {'book': book})
 
 # View to delete a book
@@ -35,4 +35,4 @@ def edit_book(request, book_id):
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     book.delete()
-    return redirect('list_books')
+    return redirect('book_list')
