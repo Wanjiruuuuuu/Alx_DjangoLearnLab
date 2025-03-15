e40028c9e0c21f47c56aedf1cbb8a93adcc2c380
Rachel <rachelwanjiru037@gmail.com>
2025-03-15 19:04:04 +0300

    pushing same changed to different directory

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('', home_redirect), 
     path('admin/', admin.site.urls),
     path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book/detail'),
+    path('books/create/', BookCreateView.as_view(), name='book/create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book/update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book/delete'),
 ]
