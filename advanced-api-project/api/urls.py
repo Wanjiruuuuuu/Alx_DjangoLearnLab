a0045ae76949f0a6ec3b62c482db4ffffec25eac
Rachel <rachelwanjiru037@gmail.com>
2025-03-15 18:59:15 +0300

    Renamed the url patterns

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,9 +5,9 @@ from api.views import BookListView, BookDetailView, BookCreateView, BookUpdateVi
 urlpatterns = [
     path('', home_redirect), 
     path('admin/', admin.site.urls),
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/', BookListView.as_view(), name='book/list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book/detail'),
+    path('books/create/', BookCreateView.as_view(), name='book/create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book/update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book/delete'),
 ]
