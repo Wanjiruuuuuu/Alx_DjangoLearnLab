b37d728e8b875d59dca740b18d21daad1300c216
Rachel <rachelwanjiru037@gmail.com>
2025-03-23 13:51:17 +0300

    Added urls.py in blog folder

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from blog import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path("register/", views.register, name="register"),
-    path("profile/", views.profile, name="profile"),
-    path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name="blog/logout.html"), name="logout"),
+    path('', include('blog.urls')),  # This makes Django look for blog/urls.py
 ]
+    
+   
