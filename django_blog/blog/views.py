4b1febde5eec93d429baaab711e09ad683fe3bc4
Rachel <rachelwanjiru037@gmail.com>
2025-03-23 13:40:24 +0300

    Added User Auth System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout # Handles user login and logouts
+from django.contrib.auth.forms import AuthenticationForm  #Built in login forms
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) # Bind form with user data
+        if form.is_valid():  # Validate form fields
+            User = form.save() # save user to the database
+            login(request, User) #automatically login the user
+        return redirect('Profile')  # Display an empty registration form
+    
+    else:
+        
+        form = UserRegisterForm() # Create an empty form
+    
+    return render(request, 'users/register.html', {'form': form}) # Render the registration form with the given template
+
+
