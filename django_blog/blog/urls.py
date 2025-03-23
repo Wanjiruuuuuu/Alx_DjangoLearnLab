f614030fb59049341faf253a66419d8ab7147657
Rachel <rachelwanjiru037@gmail.com>
2025-03-23 15:25:47 +0300

    Created Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,15 +1,30 @@
 
 from django.contrib import admin
 from django.urls import path
-from django.urls import path
 from django.contrib.auth import views as auth_views
 from blog import views
+from .views import PostListView  # Import the view you want as home
+from .views import(
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+
+)
 
 urlpatterns = [
-
+   
+    path('', PostListView.as_view(), name='home'),  # This sets 'home' as the main page
+    path('posts/', PostListView.as_view(), name='posts'),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="blog/logout.html"), name="logout"),
-
+    
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import AuthenticationForm  #Built in login forms
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
 
 @login_required
 def profile(request):
@@ -16,7 +19,7 @@ def register(request):
         if form.is_valid():  # Validate form fields
             User = form.save() # save user to the database
             login(request, User) #automatically login the user
-        return redirect('Profile')  # Display an empty registration form
+        return render(request, "blog/register.html")
     
     else:
         
@@ -24,4 +27,50 @@ def register(request):
     
     return render(request, 'users/register.html', {'form': form}) # Render the registration form with the given template
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    # ordering = ['-published_date']
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+    context_object_name = 'post'
+    # ordering = ['-published_date']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
+  
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Allow only authors to edit posts
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'    
+
+    def test_func(self):
+        Post == self.get_object()
+        return super().test_func() # Allow only authors to delete
 
