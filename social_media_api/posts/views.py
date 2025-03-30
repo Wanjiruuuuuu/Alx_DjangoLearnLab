1652a48b03fdad93dec6275cd1be5be3da0b77d9
rachelmaina <rachelwanjiru037@gmail.com>
2025-03-30 19:19:30 +0300

    Added permissions.py and views content

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for handling CRUD operations on posts.
+    Users can only edit or delete their own posts.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for handling CRUD operations on comments.
+    Users can only edit or delete their own comments.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
