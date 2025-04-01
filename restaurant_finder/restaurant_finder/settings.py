a78a9e0c5d70eadf623f6581b30deed461658cf2
rachelmaina <rachelwanjiru037@gmail.com>
2025-04-01 19:47:03 +0300

    Successfuly reset database and migrations to resolve missing tables

--- restaurant_finder/restaurant_finder/settings.py
+++ restaurant_finder/restaurant_finder/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']  # For development
+STATIC_ROOT = BASE_DIR / 'staticfiles'    # For production (collected files)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- restaurant_finder/restaurant_finder/urls.py
+++ restaurant_finder/restaurant_finder/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from django.views.generic import RedirectView
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('restaurants.urls')),
+
+    path('', TemplateView.as_view(template_name='index.html')),  # Homepage
+    path('api/', include('restaurants.urls')),  # Your API endpoints
+    path('admin/', admin.site.urls),  # Admin panel
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  
  
-]
+
